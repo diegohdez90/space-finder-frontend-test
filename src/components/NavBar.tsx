d1cfe0b45f47eec60ef7c9241c69a3c0c616ffff
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2022-05-23 14:43:36 -0500

    feat: add navbar component with Link react router component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { User } from '../models/User';
+import { Link } from 'react-router-dom';
+import Home from '../pages/Home';
+interface Props {
+  user: User | undefined
+}
+
+class NavBar extends Component<Props> {
+  render() {
+    let loginLogout: any;
+    if (this.props.user) {
+      loginLogout = <Link to='/logout'>Logout</Link>
+    } else {
+      loginLogout = <Link to='/login'>Login</Link>
+    }
+    return (
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/profile'>Profile</Link>
+        {
+          loginLogout
+        }
+      </div>
+    )
+  }
+}
+
+export default NavBar;
